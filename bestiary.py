f46002e57e26a1981e472b85bb9a4555acf042c3
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-01-16 23:12:56 -0300

    monsters data

---
+++ bestiary.py
@@ -0,0 +1,103 @@
+# Bestiary contains all mosters in the game.
+monsters = [
+    {
+        "name": "Orc",
+        "min_hp": 10,
+        "max_hp": 20,
+        "min_damage": 1,
+        "max_damage": 6,
+        "armor_modifier": -20,
+        "xp": 5,
+        "gold": 5
+    },
+    {
+        "name": "Goblin",
+        "min_hp": 10,
+        "max_hp": 30,
+        "min_damage": 1,
+        "max_damage": 10,
+        "armor_modifier": 5,
+        "xp": 3,
+        "gold": 3
+    },
+    {
+        "name": "Golem",
+        "min_hp": 45,
+        "max_hp": 80,
+        "min_damage": 5,
+        "max_damage": 25,
+        "armor_modifier": 10,
+        "xp": 10,
+        "gold": 10
+    },
+    {
+        "name": "Bear",
+        "min_hp": 35,
+        "max_hp": 75,
+        "min_damage": 20,
+        "max_damage": 75,
+        "armor_modifier": 25,
+        "xp": 7,
+        "gold": 7
+    },
+    {
+        "name": "Dragon",
+        "min_hp": 50,
+        "max_hp": 100,
+        "min_damage": 10,
+        "max_damage": 50,
+        "armor_modifier": 25,
+        "xp": 25,
+        "gold": 25
+    },
+    {
+        "name": "Basilisk",
+        "min_hp": 30,
+        "max_hp": 60,
+        "min_damage": 5,
+        "max_damage": 30,
+        "armor_modifier": 20,
+        "xp": 20,
+        "gold": 20
+    },
+    {
+        "name": "Succubus",
+        "min_hp": 20,
+        "max_hp": 40,
+        "min_damage": 5,
+        "max_damage": 20,
+        "armor_modifier": 15,
+        "xp": 15,
+        "gold": 15
+    },
+    {
+        "name": "Griffin",
+        "min_hp": 60,
+        "max_hp": 90,
+        "min_damage": 10,
+        "max_damage": 50,
+        "armor_modifier": 10,
+        "xp": 30,
+        "gold": 30
+    },
+    {
+        "name": "Doppler",
+        "min_hp": 50,
+        "max_hp": 100,
+        "min_damage": 20,
+        "max_damage": 60,
+        "armor_modifier": 25,
+        "xp": 35,
+        "gold": 35
+    },
+    {
+        "name": "Harpy",
+        "min_hp": 20,
+        "max_hp": 50,
+        "min_damage": 15,
+        "max_damage": 30,
+        "armor_modifier": 40,
+        "xp": 12,
+        "gold": 12
+    },
+]
