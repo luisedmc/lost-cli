40a1634fa7ed5ef2f273e93772e648e7664ad234
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-01-17 08:59:42 -0300

    const variables (map, player)

---
+++ config.py
@@ -0,0 +1,22 @@
+PLAYER_HP: int = 100
+
+
+# Map Size
+MAX_X_AXIS: int = 10
+MAX_Y_AXIS: int = 5
+
+"""
+                Map Design
+
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+X X X X X X X X X X 0 X X X X X X X X X X
+"""
