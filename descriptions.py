c09c03cc6c28e0ec406beea1360cd3d403b8e5bc
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-01-19 06:54:43 -0300

    island descriptions

---
+++ descriptions.py
@@ -0,0 +1,95 @@
+from colorama import Fore
+
+
+# Description contains the descriptions for the various islands in the game.
+descriptions = (
+    Fore.GREEN +
+    """
+        This island is covered in a thick layer of sand, and the only vegetation is a few small cacti.""",
+
+    Fore.GREEN +
+    """
+        You can see that beautiful blue ocean water surrounds this island, and the sand is a bit finer than the last.""",
+    
+    Fore.GREEN +
+    """
+        A lot of small trees grow on this island. The ocean water is a bit darker than the last island.""",
+
+    Fore.GREEN +
+    """
+        A very strange vegetation grows on this island. It looks like a cross between a cactus and a tree.""",
+
+    Fore.GREEN +
+    """
+        It is impossible to be sure, but it looks like this island is covered in a thick layer of snow.""",
+
+    Fore.GREEN +
+    """
+        The most strange thing about this island is the water color. It is a deep red.""",
+
+    Fore.GREEN +
+    """
+        The sun is shining brightly on this island, and the sand is a very light color.""",
+
+    Fore.GREEN +
+    """
+        Rain falls on this island. The entire island is covered in a thick layer of mud.""",
+)
+
+# Sounds contains the various sounds that can be found in the game.
+sounds = (
+    Fore.GREEN +
+    """
+        Birds chirp in the distance, but you can't see them anywhere.""",
+
+    Fore.GREEN +
+    """
+        The faint sound of something moving in the distance is all you can hear.""",
+
+    Fore.GREEN +
+    """
+        You're not certain, but you think you hear the clicking of talons against the hot sand distance.""",
+
+    Fore.GREEN +
+    """
+        The faint echo of something suspiciously like a growl comes from further out in the deep sea.""",
+
+    Fore.GREEN +
+    """
+        You hear a faint grunting noise from somewhere in the distance.""",
+    
+    Fore.GREEN +
+    """
+        What sounds like faint laughter wends its way into the near distance.""",
+
+    Fore.GREEN +
+    """
+        You're not sure, but something like a faint hiss seems to sound off in the distance.""",
+)
+
+# Smells contains the various smells that can be found in the game.
+smells = (
+    Fore.GREEN +
+    """
+        There is a faint smell of rotting vegetation in the air.""",
+
+    Fore.GREEN +
+    """
+        Given the stench in the air, something must have died nearby recently.""",
+
+    Fore.GREEN +
+    """
+        The sea air smells of salt and fish. Probably a good place for fishing.""",
+
+    Fore.GREEN +
+    """
+        The coppery smell of blood hangs in the air.""",
+
+    Fore.GREEN +
+    """
+        You are not certain what is causing the foul stench in the air, but you have your suspicions...""",
+
+    Fore.GREEN +
+    """
+        The smell is so strong. Someone or something was here recently.""",
+)
